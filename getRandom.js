200f5c1c26caecb119b0afc228c3a85a09b16271
junppark <junppark0612@gmail.com>
2021-09-26 09:12:08 -0400

    start project

--- getRandom.js
+++
@@ -1,7 +0,0 @@
-export  function random() {
-  return Math.floor(Math.random() * 10);
-}
-export const user = {
-  name: 'sangjun',
-  age: 22
-}
--- getType.js
+++
@@ -1,3 +0,0 @@
-export default function /*getType*/(data) {  // since it is default, the function name does not necessrary
-  return Object.prototype.toString.call(data).slice(8, -1)
-}
